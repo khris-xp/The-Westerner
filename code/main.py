9dca82f830858fe547fbbba01d111e023af26389
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-10-12 09:59:30 +0700

    add : player[2]

--- code/main.py
+++ code/main.py
@@ -1,39 +1,40 @@
-import pygame
-import sys
-from settings import *
+import pygame, sys
+from settings import * 
 from player import Player
 
-
 class Game:
-    def __init__(self):
-        pygame.init()
-        self.display_surface = pygame.display.set_mode(
-            (WINDOW_WIDTH, WINDOW_HEIGHT))
-        pygame.display.set_caption('The Westerner')
-        self.clock = pygame.time.Clock()
+	def __init__(self):
+		pygame.init()
+		self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+		pygame.display.set_caption('Western shooter')
+		self.clock = pygame.time.Clock()
 
-        self.all_sprites = pygame.sprite.Group()
-        self.setup()
+		# groups 
+		self.all_sprites = pygame.sprite.Group()
 
-    def setup(self):
-        Player((200, 200), self.all_sprites, None, None)
+		self.setup()
 
-    def run(self):
-        while True:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
-            dt = self.clock.tick() / 1000  # dt in milliseconds
+	def setup(self):
+		Player((200,200), self.all_sprites, PATHS['player'], None)
 
-            # Update
-            self.all_sprites.update(dt)
+	def run(self):
+		while True:
+			# event loop 
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+			dt = self.clock.tick() / 1000
 
-            # Draw image
-            self.display_surface.fill('black')
-            self.all_sprites.draw(self.display_surface)
-            pygame.display.update()
+			# update groups 
+			self.all_sprites.update(dt)
+
+			# draw groups
+			self.display_surface.fill('black')
+			self.all_sprites.draw(self.display_surface)
+
+			pygame.display.update()
 
 if __name__ == '__main__':
-    game = Game()
-    game.run()
+	game = Game()
+	game.run()
--- code/player.py
+++ code/player.py
@@ -1,12 +1,18 @@
+from locale import currency
 import pygame
 from pygame.math import Vector2 as vector
+from os import walk
 
 
 class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, path, collision_sprites):
         super().__init__(groups)
-        self.image = pygame.Surface((100, 100))
-        self.image.fill('red')
+
+        self.import_assets(path)
+        self.frame_index = 0
+        self.status = 'down_idle'
+
+        self.image = self.animations[self.status][self.frame_index]
         self.rect = self.image.get_rect(center=pos)
 
         # Based Movement
@@ -18,25 +24,43 @@ class Player(pygame.sprite.Sprite):
         self.hitbox = self.rect.inflate(0, -self.rect.height / 2)
         self.coliision_sprites = collision_sprites
 
+    def import_assets(self, path):
+        self.animations = {}
+
+        for index, folder in enumerate(walk(path)):
+            if index == 0:
+                for name in folder[1]:
+                    self.animations[name] = []
+            else:
+                for file_name in sorted(folder[2], key=lambda string: int(string.split('.')[0])):
+                    path = folder[0].replace('\\', '/') + '/' + file_name
+                    surf = pygame.image.load(path).convert_alpha()
+                    key = folder[0].split('\\')[1]
+                    self.animations[key].append(surf)
+
     def input(self):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_d]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_a]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction.x = 0
 
         if keys[pygame.K_w]:
-            self.direction.y  = -1
+            self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_s]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction.y = 0
 
     def move(self, dt):
-        
+
         # Another Angle Draw (normalize)
         if self.direction.magnitude() != 0:
             self.direction = self.direction.normalize()
@@ -49,6 +73,16 @@ class Player(pygame.sprite.Sprite):
         self.hitbox.centery = round(self.pos.y)
         self.rect.centery = self.hitbox.centery
 
+    def animate(self, dt):
+        current_animation = self.animations[self.status]
+
+        self.frame_index += 7 * dt
+        if self.frame_index >= len(current_animation):
+            self.frame_index = 0
+
+        self.image = current_animation[int(self.frame_index)]
+
     def update(self, dt):
         self.input()
         self.move(dt)
+        self.animate(dt)
