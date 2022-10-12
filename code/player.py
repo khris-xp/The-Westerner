a418d6c0c9ad1a7bdf218bef9805d95ac38f6d98
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-10-12 14:37:34 +0700

    add : player [3]

--- code/player.py
+++ code/player.py
@@ -24,6 +24,16 @@ class Player(pygame.sprite.Sprite):
         self.hitbox = self.rect.inflate(0, -self.rect.height / 2)
         self.coliision_sprites = collision_sprites
 
+        # Attack
+        self.attacking = False
+
+    def get_status(self):
+        if self.direction.x == 0 and self.direction.y == 0:
+            self.status = self.status.split('_')[0] + '_idle'
+        
+        if self.attacking:
+            self.status = self.status.split('_')[0] + '_attack'
+
     def import_assets(self, path):
         self.animations = {}
 
@@ -41,23 +51,29 @@ class Player(pygame.sprite.Sprite):
     def input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_d]:
-            self.direction.x = 1
-            self.status = 'right'
-        elif keys[pygame.K_a]:
-            self.direction.x = -1
-            self.status = 'left'
-        else:
-            self.direction.x = 0
+        if not self.attacking:
+            if keys[pygame.K_d]:
+                self.direction.x = 1
+                self.status = 'right'
+            elif keys[pygame.K_a]:
+                self.direction.x = -1
+                self.status = 'left'
+            else:
+                self.direction.x = 0
 
-        if keys[pygame.K_w]:
-            self.direction.y = -1
-            self.status = 'up'
-        elif keys[pygame.K_s]:
-            self.direction.y = 1
-            self.status = 'down'
-        else:
-            self.direction.y = 0
+            if keys[pygame.K_w]:
+                self.direction.y = -1
+                self.status = 'up'
+            elif keys[pygame.K_s]:
+                self.direction.y = 1
+                self.status = 'down'
+            else:
+                self.direction.y = 0
+
+            if keys[pygame.K_SPACE]:
+                self.attacking = True
+                self.direction = vector()
+                self.frame_index = 0
 
     def move(self, dt):
 
@@ -79,6 +95,8 @@ class Player(pygame.sprite.Sprite):
         self.frame_index += 7 * dt
         if self.frame_index >= len(current_animation):
             self.frame_index = 0
+            if self.attacking:
+                self.attacking = False
 
         self.image = current_animation[int(self.frame_index)]
 
@@ -86,3 +104,4 @@ class Player(pygame.sprite.Sprite):
         self.input()
         self.move(dt)
         self.animate(dt)
+        self.get_status()
